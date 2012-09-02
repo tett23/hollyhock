5cfb128a990a88f89b84605333b6721699852adf
tett23 <tett23@gmail.com>
2012-09-03 01:19:29 +0900

    ApplicationConfigにvalidateを追加などなど

--- models/application_config.rb
+++ models/application_config.rb
@@ -1,10 +1,16 @@
+#encoding: utf-8
+
 class ApplicationConfig
   include DataMapper::Resource
 
   # property <name>, <type>
   property :id, Serial
-  property :name, String
+  property :name, String, :required=>true, :unique=>true, :unique_index=>true, :messages=>{
+    :is_unique=>"値はすでに使われています"
+  }
   property :value, Text
+
+  validates_is_unique :name, :scope=>:value
 
   def self.value(name)
     config = first(:name=>name)
